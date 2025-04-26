3edb72a4b5d218bd8dbba2c7d4c6b0ba4d75d446
Stephen Riley <jstephen.riley@yahoo.com>
2025-04-26 16:42:06 -0700

    added double -> Rational, Leibniz pi calculation

--- src/Rational.cs
+++ src/Rational.cs
@@ -40,7 +40,6 @@ public sealed record Rational
         var b = r.Mul(lcm / r.Denom, simplify: false);
         return From(a.Num + b.Num, a.Denom, simplify);
     }
-
     public Rational Sub(Rational r, bool simplify = true) => Add(r.Negate(simplify), simplify);
 
     public Rational Mul(Rational r, bool simplify = true)
@@ -96,5 +95,106 @@ public sealed record Rational
             }
         }
         return num2;
+    }
+
+    private const double DEFAULT_PRECISION = 0.0001;
+
+    public static implicit operator Rational(double value) => DoubleToRational(value, DEFAULT_PRECISION);
+
+    // from https://stackoverflow.com/a/32903747
+    public static Rational DoubleToRational(double value, double accuracy = DEFAULT_PRECISION)
+    {
+        if (accuracy is >= 1.0 or <= 0.0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(accuracy), "Must be > 0 and < 1.");
+        }
+
+        int sign = Math.Sign(value);
+
+        if (sign == -1)
+        {
+            value = Math.Abs(value);
+        }
+
+        // Accuracy is the maximum relative error; convert to absolute maxError
+        double maxError = sign == 0 ? accuracy : value * accuracy;
+
+        int n = (int)Math.Floor(value);
+        value -= n;
+
+        if (value < maxError)
+        {
+            return Rational.From(sign * n, 1, simplify: false);
+        }
+
+        if (1 - maxError < value)
+        {
+            return Rational.From(sign * (n + 1), 1, simplify: false);
+        }
+
+        // The lower fraction is 0/1
+        int lower_n = 0;
+        int lower_d = 1;
+
+        // The upper fraction is 1/1
+        int upper_n = 1;
+        int upper_d = 1;
+
+        while (true)
+        {
+            // The middle fraction is (lower_n + upper_n) / (lower_d + upper_d)
+            int middle_n = lower_n + upper_n;
+            int middle_d = lower_d + upper_d;
+
+            if (middle_d * (value + maxError) < middle_n)
+            {
+                // real + error < middle : middle is our new upper
+                Seek(ref upper_n, ref upper_d, lower_n, lower_d, (un, ud) => (lower_d + ud) * (value + maxError) < (lower_n + un));
+            }
+            else if (middle_n < (value - maxError) * middle_d)
+            {
+                // middle < real - error : middle is our new lower
+                Seek(ref lower_n, ref lower_d, upper_n, upper_d, (ln, ld) => (ln + upper_n) < (value - maxError) * (ld + upper_d));
+            }
+            else
+            {
+                // Middle is our best fraction
+                return From((n * middle_d + middle_n) * sign, middle_d, simplify: false);
+            }
+        }
+    }
+
+    internal static void Seek(ref int a, ref int b, int ainc, int binc, Func<int, int, bool> f)
+    {
+        a += ainc;
+        b += binc;
+
+        if (f(a, b))
+        {
+            int weight = 1;
+
+            do
+            {
+                weight *= 2;
+                a += ainc * weight;
+                b += binc * weight;
+            }
+            while (f(a, b));
+
+            do
+            {
+                weight /= 2;
+
+                int adec = ainc * weight;
+                int bdec = binc * weight;
+
+                if (!f(a - adec, b - bdec))
+                {
+                    a -= adec;
+                    b -= bdec;
+                }
+            }
+            while (weight > 1);
+        }
     }
 }
--- tests/CastOperatorTests.cs
+++ tests/CastOperatorTests.cs
@@ -22,24 +22,54 @@ public class CastOperatorTests
         Assert.AreEqual(0.6666666666666666d, d);
     }
 
-    // [TestMethod]
+    [TestMethod]
+    [DataRow(0.25d, 1, 4)]
+    [DataRow(0.333333333333333333d, 1, 3)]
+    [DataRow(1d, 9, 9)]
+    public void DoubleToRationalCast(double d, long xNum, long xDenom)
+    {
+        Rational r = d;
+        var x = Rational.From(xNum, xDenom);
+        Assert.AreEqual(x, r);
+    }
+
+    // Just for fun, let's try the Leibniz formula for π
+    [TestMethod]
     public void PiCast()
     {
         var add = true;
         var r = Rational.Zero();
 
-        const int COUNT = 100;
+        const int COUNT = 1_000;
 
         for (var i = 1; i < COUNT; i += 2)
         {
-            var term = Rational.From(4, i);
-            term = add ? term : term.Negate();
+            var term = Rational.From(add ? 4 : -4, i);
             r = r.Add(term);
-            Debug.WriteLine($"at {i}: {r}");
             add = !add;
+
+            // uncomment to get a running estimate
+            Debug.WriteLine($"at {i}: {r} ({(double)r})");
+
+            // due to long overflow of the denominator,
+            //  let's convert to double then convert back
+            //  to see if we can reduce the denominator
+            //  every once in a while
+            if ((i - 1) % 10 == 0)
+            {
+                var approx = (double)r;
+                r = approx;
+            }
         }
 
-        double d = r;
-        Assert.AreEqual(Math.PI, d);
+        // unless we convert Rational to use BigInteger,
+        //  we have to be satisfied with only a few decimal
+        //  places of accuracy, even after 1000 iterations.
+        const double TEST_PRECISION = 0.001;
+
+        double target = Math.Floor(Math.PI / TEST_PRECISION) * TEST_PRECISION;
+        double d = Math.Round(r / TEST_PRECISION) * TEST_PRECISION;
+
+        Assert.AreEqual(target, d);
     }
 }
